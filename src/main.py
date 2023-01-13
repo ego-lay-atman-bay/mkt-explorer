4e5c0550665ea26d1f8f71e9742a3e02b73bf287
ego-lay-atman-bay <70973578+ego-lay-atman-bay@users.noreply.github.com>
2023-01-13 12:57:10 -0700

    Start creating ui

--- src/main.py
+++ src/main.py
@@ -5,16 +5,72 @@ import tkinter.ttk as ttk
 import tkinter.filedialog as filedialog
 
 class Window(tk.Tk):
-    def __init__(self, *args, **kwargs):
+    def __init__(this, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.title('')
-        self.geometry('%dx%d' % (760 , 610) )
+        this.title('')
+        this.geometry('%dx%d' % (760 , 610) )
+        
+        this.files = []
+        this.assets = []
+        this.fileStructure = {}
 
-        self.initialize()
+        this.notebook = ttk.Notebook()
+        this.notebook.pack(fill='both', expand=True)
+        
+        this.pages = {
+            'assets': {
+                'frame': ttk.Frame(this.notebook),
+                'contents': {}
+            }
+        }
+        
+        this.notebook.add(this.pages['assets']['frame'], text='Assets')
+        
+        this.createAssets()
+        
+        this.initialize()
+        
+    def createMenuBar(this):
+        this.menuBar = tk.Menu(this)
+        this.config(menu=this.menuBar)
 
-    def initialize(self):
+        this.fileMenu = tk.Menu(this.menuBar, tearoff=0)
+        this.fileMenu.add_command(label= 'Load File')
+        this.fileMenu.add_command(label= 'Load Folder')
+        this.fileMenu.add_separator()
+        this.fileMenu.add_command(label='Extract File')
+        this.fileMenu.add_command(label='Extract Folder')
+
+        this.menuBar.add_cascade(label= 'File', menu=this.fileMenu)
+
+    def initialize(this):
         pass
+    
+    def createAssets(this):
+        this.pages['assets']['columns'] = ['name', 'container', 'type', 'pathID', 'size']
+        this.pages['assets']['contents']['treeview'] = ttk.Treeview(this.pages['assets']['frame'], columns=this.pages['assets']['columns'], show='headings')
+        
+        columnWidth = 1
+        
+        this.pages['assets']['contents']['treeview'].heading('name', text='Name', anchor='w')
+        this.pages['assets']['contents']['treeview'].column('name', width=columnWidth)
 
+        this.pages['assets']['contents']['treeview'].heading('container', text='Container', anchor='w')
+        this.pages['assets']['contents']['treeview'].column('container', width=columnWidth)
+
+        this.pages['assets']['contents']['treeview'].heading('type', text='Type', anchor='w')
+        this.pages['assets']['contents']['treeview'].column('type', width=columnWidth)
+
+        this.pages['assets']['contents']['treeview'].heading('pathID', text='PathID', anchor='w')
+        this.pages['assets']['contents']['treeview'].column('pathID', width=columnWidth)
+
+        this.pages['assets']['contents']['treeview'].heading('size', text='Size', anchor='w')
+        this.pages['assets']['contents']['treeview'].column('size', width=columnWidth)
+        
+        this.pages['assets']['contents']['treeview'].pack(fill='both', expand=True)
+        
+        
+        
 def main():
     app = Window()
     app.mainloop()
